5dd3ea6c0b2ae0d9eef6b8bb95152d990a3bb93a
Venkatesh Kanchan <venks@broadweb.com.au>
2020-04-25 23:12:06 +1000

    Remove spurious characters &amp; and add permissions after running docker-entrypoint.sh as that command is what copies wordpress from /usr/src/wordpress to /var/www/html

--- entrypoint-child.sh
+++ entrypoint-child.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+exec docker-entrypoint.sh "$@"
+
 echo "Adding permissions"
-chown -R www-data:www-data /var/www/html/wp-content &amp;
-find /var/www/html/wp-content -type d -exec chmod 0755 {} \; &amp;
-find /var/www/html/wp-content -type f -exec chmod 644 {} \; &amp;
+chown -R www-data:www-data /var/www/html/wp-content 
+find /var/www/html/wp-content -type d -exec chmod 0755 {} \; 
+find /var/www/html/wp-content -type f -exec chmod 644 {} \; 
 echo "Permissions added"
 
-exec docker-entrypoint.sh "$@"
+
